45ba7840cd3732b6eb176703b9a34974d1710e14
JavCan <javicanella69@gmai.com>
2025-12-10 17:43:23 -0600

    correccion de versiones

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { useTheme } from "next-themes@0.4.6";
-import { Toaster as Sonner, ToasterProps } from "sonner@2.0.3";
+import { useTheme } from "next-themes";
+import { type ToasterProps as SonnerToasterProps } from "sonner";
 
-const Toaster = ({ ...props }: ToasterProps) => {
+const Toaster = ({ ...props }: SonnerToasterProps) => {
   const { theme = "system" } = useTheme();
 
   return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
+    <Toaster
+      theme={theme as SonnerToasterProps["theme"]}
       className="toaster group"
       style={
         {
